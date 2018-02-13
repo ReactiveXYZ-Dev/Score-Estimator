db7808f1f6ac645a82b6489910f2c12b227e511b
Ruowang(Jackie) Zhang <dev@reactive.xyz>
2018-02-13 13:55:30 -0500

    v1 complete

---
+++ js/README.md
@@ -0,0 +1,14 @@
+### A simple class grade estimator
+
+This web tool will simply tell you how well you need to do after all existing assignments.
+
+You can **lock** assignments to set the score you've or assumed to achieve, and it can calculate how much you need to do for other assignments.
+
+You can even save and restore your grade status and revisit it next time after you got a new grade back.
+
+This is just a fun (and sad every time I calculate my score) tool that I build for my use, but I believe it is adapable for most college courses.
+
+Enjoy and happy studying!
+
+**Notice**:
+IN ORDER TO CALCULATE ASSIGNMENT ESTIMATION, YOU MUST MAKE SURE THAT THE WEIGHTS OF YOUR ASSIGNMENTS ADD UP TO ONE!
--- js/main.js
+++ js/main.js
@@ -68,6 +68,36 @@ new Vue({
 		},
 
 		/**
+		 * Calculate scores for unlocked assignments based on locked assignments
+		 * @return {Void}
+		 */
+		calculateScores() {
+			// check goal validity
+			if (!this.goalScore) {
+				alert("Please enter your goal!");
+				return;
+			}
+			// loop through assignments and compare overall percentage with locked ones
+			var lockedScore = 0.0;
+			for (var i = 0; i < this.assignmentData.length; ++i) {
+				var weight = parseFloat(this.assignmentData[i].weight);
+				var score = parseFloat(this.assignmentData[i].score);
+				if (this.assignmentData[i].isLocked) {
+					lockedScore += weight * score/100;
+				} 
+			}
+			var restPercentage = 100 - lockedScore;
+
+			// distribute the rest percentage to all unlocked score
+			for (var i = 0; i < this.assignmentData.length; ++i) {
+				if (!this.assignmentData[i].isLocked) {
+					var weight = parseFloat(this.assignmentData[i].weight)/100;
+					this.assignmentData[i].score = weight * restPercentage;
+				}
+			}
+		},
+
+		/**
 		 * Save assignment data to local storage
 		 * @return {Void}
 		 */
@@ -76,6 +106,8 @@ new Vue({
 			localStorage.setItem("COURSE_DATA_" + courseName, JSON.stringify(this.assignmentData));
 			if (courseName) {
 				alert("Saved!");
+				// reload backups
+				this.backups = this._loadBackups();
 			} else {
 				alert("Please. Enter the course name");
 			}
@@ -93,13 +125,22 @@ new Vue({
 		 * Restore assignments to UI
 		 * @return {Void}
 		 */
-		restoreAssignments() {
+		restoreBackup() {
 			if (this.selectedBackup) {
 				this.assignmentData = JSON.parse(localStorage.getItem(this.selectedBackup));
 				$('#restore-modal').modal('hide');
 			} else {
 				alert("Please. Select a backup!");
 			}
+		},
+
+		/**
+		 * Delete an existing data backup
+		 * @return {Void}
+		 */
+		deleteBackup(backup, idx) {
+			localStorage.removeItem(backup.key);
+			this.backups.splice(idx, 1);
 		},
 
 		/**
@@ -127,7 +168,7 @@ new Vue({
 			if (totalWeight == 0) {
 				return "--";
 			}
-			return totalScore / totalWeight;
+			return (totalScore / totalWeight).toFixed(2);
 		},
 
 		/**
@@ -152,7 +193,7 @@ new Vue({
 			if (totalWeight == 0) {
 				return "--";
 			}
-			return totalScore / totalWeight;
+			return (totalScore / totalWeight).toFixed(2);
 		},
 
 		/**
@@ -162,22 +203,32 @@ new Vue({
 		 */
 		_isNumber(str) {
 			return str!="" && !isNaN(str);
+		},
+
+		/**
+		 * Load backups from local storage
+		 * @return {Array}
+		 */
+		_loadBackups() {
+			var i = 0, results = [];
+			while (localStorage.key(i)) {
+				var keyName = localStorage.key(i);
+				var idx = keyName.indexOf("COURSE_DATA_")
+				if (idx != -1) {
+					results.push({
+						courseName: keyName.slice(idx + "COURSE_DATA_".length),
+						key: keyName
+					});
+				}
+				++i;
+			}
+
+			return results;
 		}
 
 	},
 	mounted: function() {
 		// load existing backups if any
-		var i = 0;
-		while (localStorage.key(i)) {
-			var keyName = localStorage.key(i);
-			var idx = keyName.indexOf("COURSE_DATA_")
-			if (idx != -1) {
-				this.backups.push({
-					courseName: keyName.slice(idx + "COURSE_DATA_".length),
-					key: keyName
-				});
-			}
-			++i;
-		}
+		this.backups = this._loadBackups();
 	}
 });
